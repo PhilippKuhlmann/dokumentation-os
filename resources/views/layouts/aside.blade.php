3aa97b0f5436ffe4c3d0eda6fbc4161a3db2b5a7
PhilippKuhlmann <p.kuhlmann@me.com>
2023-05-24 18:57:48 +0200

    -

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -42,7 +42,6 @@
                     </x-slot:svg>
                     <div class="flex justify-between">
                         VMs
-                        <span class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-gray-600 dark:text-gray-100">{{ $customer->vms()->count() }}</span>
                     </div>
                 </x-aside.link>
 
