07b28c5d313c4500485fb3f847a33cf8f459fb98
PhilippKuhlmann <p.kuhlmann@me.com>
2023-10-31 11:18:41 +0100

    fix

--- database/migrations/2023_10_28_073521_create_rooms_table.php
+++ database/migrations/2023_10_28_073521_create_rooms_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('site_id')->constrained('sites')->onDelete('cascade');
             $table->string('name');
-            $table->string('floor');
+            $table->string('floor')->nullable();
             $table->timestamps();
         });
     }
